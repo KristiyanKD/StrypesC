a3b79fbd975ee6476ce486ec926de490e6677cf2
KristiyanKD <100771549+KristiyanKD@users.noreply.github.com>
2022-08-29 15:48:52 +0300

    Add files via upload

---
+++ Homework4/zad4.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+int main()
+{
+    int n;
+
+    float temp, sqrt;
+    printf("n = ");
+    scanf("%d", &n);
+    sqrt = n / 2;
+    temp = 0;
+    while(sqrt != temp){
+        temp = sqrt;
+
+        sqrt = ( n/temp + temp) / 2;
+    }
+
+    printf("sqrt = %.2f\n", sqrt);
+}
