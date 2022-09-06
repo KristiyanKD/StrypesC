ca1fcf50cd64448e8d231f9185e801e405fa3acb
KristiyanKD <100771549+KristiyanKD@users.noreply.github.com>
2022-09-06 14:18:50 +0300

    Update zad4.c

--- Homework2/zad4.c
+++ Homework2/zad4.c
@@ -3,20 +3,20 @@
 
 int quadEq(double a, double b, double c, double* x1, double* x2)
 {
-    double D = b * b - 4 * a * c;
+    double D = (b * b) - (4 * a * c);
 
     if(D < 0)
     {
         printf("No real roots\n");
         return -1;
     }
-    if(D==0)
+    if(D == 0)
     {
         *x1 = *x2 = (-b) / (2 * a);
         printf("x1 = x2 = %.2lf\n", *x1);
         return 0;
     }
-    if(D>0)
+    if(D > 0)
     {
         *x1 = ((-b) + sqrt(D)) / (2 * a);
         *x2 = ((-b) - sqrt(D)) / (2 * a);
