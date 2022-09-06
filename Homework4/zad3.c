47ad07cfdce6974f64ee10aba3884d45857c2450
KristiyanKD <100771549+KristiyanKD@users.noreply.github.com>
2022-09-06 14:11:27 +0300

    Update zad3.c

--- Homework4/zad3.c
+++ Homework4/zad3.c
@@ -8,7 +8,7 @@ double randDouble(void)
 }
 double randDoubleInterval(const double a, const double b)
 {
-   return randDouble()*(b-a) + a;
+   return randDouble() * (b - a) + a;
 }
  
 int main()
@@ -17,37 +17,37 @@ int main()
     int n, m, p;
     printf("n = ");
     scanf("%d", &n);
-    double* arr = malloc(n*sizeof(double));
+    double* arr = malloc(n * sizeof(double));
     if (arr == NULL){
         return EXIT_FAILURE;
     }
-    int i=0;
-    for( ; i<n; ++i)
+    int i = 0;
+    for( ; i < n; ++i)
     {
         arr[i] = randDoubleInterval(0.0, 1.0);
     }
     printf("m = ");
     scanf("%d", &m);
-    arr = realloc(arr, (n+m)*sizeof(double));
+    arr = realloc(arr, (n + m) * sizeof(double));
     if (arr == NULL){
         return EXIT_FAILURE;
     }
-    for( ; i<(n+m); ++i)
+    for( ; i < (n + m); ++i)
     {
         arr[i] = randDoubleInterval(1.0, 2.0);
     }
     printf("p = ");
     scanf("%d", &p);
-    arr = realloc(arr, (n+m+p)*sizeof(double));
+    arr = realloc(arr, (n + m + p) * sizeof(double));
     if (arr == NULL){
         return EXIT_FAILURE;
     }
-    for( ; i<(n+m+p); ++i)
+    for( ; i < (n + m + p); ++i)
     {
         arr[i] = randDoubleInterval(2.0, 3.0);
     }
  
-    for(i=0 ; i<(n+m+p); ++i)
+    for(i=0 ; i < (n + m + p); ++i)
     {
         printf("arr[%d] = %.2lf\n", i, arr[i]);
     }
