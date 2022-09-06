566349a2300368d957ec381f1afd464599a9d91c
KristiyanKD <100771549+KristiyanKD@users.noreply.github.com>
2022-09-06 13:57:54 +0300

    Update zad2.c

--- Homework3/zad2.c
+++ Homework3/zad2.c
@@ -6,23 +6,22 @@ unsigned sumArrayDigits(const int*, size_t);
 
 int main()
 {
-    int arr[]={19,28,64,55,4,6,37};
-    printf("%d\n", sumArrayDigits(arr, 7));
+    int arr[]={19, 28, 64, 55, 4, 6, 37};
+    printf("sum = %d\n", sumArrayDigits(arr, 7));
     
-
     return 0;
 }
 
 unsigned digitsSum(int n)
 {
-    if(n<10){
+    if(n < 10){
         return n;
     }
     unsigned sum = 0;
-    while(n!=0)
+    while(n != 0)
     {
-        sum += n%10;
-        n/=10;
+        sum += n % 10;
+        n /= 10;
     }
     return sum;
 }
@@ -30,9 +29,9 @@ unsigned digitsSum(int n)
 unsigned sumArrayDigits(const int* arr, size_t n)
 {
     unsigned sum = 0;
-    for(int i=0;i < n; ++i)
+    for(int i = 0; i < n; ++i)
     {
-        sum += digitsSum(*(arr+i));
+        sum += digitsSum(*(arr + i));
     }
     return sum;
 }
