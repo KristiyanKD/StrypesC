94b01a91a5df3ad6c0832deeeaae939561b73a3f
KristiyanKD <100771549+KristiyanKD@users.noreply.github.com>
2022-09-06 14:22:06 +0300

    Update zad5.c

--- Homework2/zad5.c
+++ Homework2/zad5.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 #include<stdint.h>
 
-unsigned onesCount(uint64_t mask)
+unsigned onesCount(const uint64_t mask)
 {
     unsigned count = 0;
     for (int i = 0; i < 64; ++i)
     {
-        if(mask & (1<<i))
+        if(mask & (1ull << i))
         {
             ++count;
         }
@@ -19,15 +19,7 @@ int main()
     uint64_t num;
     printf("n = ");
     scanf("%lu", &num);
-    //do not know why but mask contains 2 times more 1's bits than expected
-    printf("1's count: %d\n",onesCount(num)/2);
-    // I am able to see them with this loop
-    for (int i = 63; i >= 0 ; --i)
-    {
-        printf("%d", num&(1<<i)?1:0);
-    
-    }
-    printf("\n");
+    printf("1's count: %d\n", onesCount(num));
 
     return 0;
 }
